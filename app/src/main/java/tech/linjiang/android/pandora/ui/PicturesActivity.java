397927756bba81b5b044ea0b75de931953122742
tigermei <tigermei@tencent.com>
2021-11-23 17:51:55 +0800

    添加picture的activity

---
+++ app/src/main/java/tech/linjiang/android/pandora/ui/PicturesActivity.java
@@ -0,0 +1,16 @@
+package tech.linjiang.android.pandora.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import tech.linjiang.android.pandora.R;
+
+public class PicturesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_picture);
+    }
+}
